85b56786c974fbfab4f7457ef009a6f2a11e55b8
NicArellano <ti.arellano.d@gmail.com>
2023-11-26 19:44:42 -0300

    avanzo algo con style

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { SocialButton } from './components/SocialButton'
 function App () {
 
 return (
-<div>
-<SocialButton /> 
-<Formulario/>
+<div className='bg-success-subtle d-flex flex-row align-item-center gap-5' >
+
+<div className='d-flex align-item-center'><SocialButton /></div>
+
+<div className=''><Formulario/></div>
 
 
 </div>
--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -1,6 +1,10 @@
 export const SocialButton = (props) => {
 
     return (
+        <div>
         <i class="fa-brands fa-facebook">{props.text}</i> 
+        <i class="fa-brands fa-github"></i>{props.tex}
+        <i class="fa-brands fa-linkedin-in"></i>{props.text}
+        </div>
     )
 }
