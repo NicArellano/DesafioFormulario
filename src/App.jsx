1453d1f6962a05be28c1f6b86720b3e557de321f
NicArellano <ti.arellano.d@gmail.com>
2023-11-26 18:21:55 -0300

    avanzamos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {Formulario} from './components/Formulario'
 import { SocialButton } from './components/SocialButton'
 
+
 function App () {
 
 return (
 <div>
 <SocialButton /> 
-<Formulario />
+<Formulario/>
+
 
 </div>
 )
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,18 @@
  export const Formulario = (props) => {
     return (
-    
+            <div>
           <form>
             <input type="text" placeholder="Nombre"/> {props.text}
-            <input type="text" placeholder="Email"/> {props.text}
+            <input type="text" placeholder="tuemail@ejemplo.com"/> {props.text}
             <input type="text" placeholder="Contraseña"/> {props.text}
-            <input type="text" placeholder="Repite Contraseña" /> {props.text} 
+            <input type="text" placeholder="Confirma tu Contraseña" /> {props.text} 
             </form>
-        
+            <button type="submit" value="Registrate" >Registrate </button>
+            </div>
       )
 
  }
+
 
 
 
